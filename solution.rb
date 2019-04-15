33403aeb088067e7514c88fcd9d5a077a22d4c82
Jose Elera <jelera@gmail.com>
2019-04-15 10:22:13 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,49 @@
+require 'pry'
 class Deck
+  attr_reader :cards
+
+
+  def initialize
+    @cards = []
+    @suit = %w(Hearts Clubs Diamonds Spades)
+    @rank = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+    @suit.each do |suit|
+      @rank.each do |rank|
+        @cards << Card.new(suit, rank)
+      end
+    end
+
+    @cards
+  end
+
+  def choose_card
+    @cards.slice!(@cards.index(@cards.sample))
+  end
+
 
 end
 
 class Card
-
+  attr_reader :suit, :rank
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+    self
+  end
 end
+
+# deck = Deck.new
+# binding.pry
+# ""
+    # @cards = []
+
+    # @suit = %w(Hearts Clubs Diamonds Spades)
+    # @rank = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+
+    # @suit.each do |suit|
+    #   @rank.each do |rank|
+    #     @cards << [suit, rank]
+    #   end
+    # end
+
+    # @cards
